291bb4611aa47fba4fda32dfcc601553589b2b9a
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-04-22 00:31:48 -0400

    update to correct formatting

--- firebase_setup.py
+++ firebase_setup.py
@@ -4,23 +4,31 @@ from firebase_admin import firestore
 
 import json
 
+with open("camera_info.json") as f:
+    data = json.load(f)
+
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
-with open("camera_info.json") as f:
-    data = json.load(f)
-
 batch = db.batch()
-for cam_id, cam_data in data.items():
-    doc_ref = (
-        db.collection("schools")
-          .document("UMD")
-          .collection("cameras")
-          .document(cam_id)
-    )
-    batch.set(doc_ref, cam_data)
+
+cameras_col = db.collection('schools').document('UMD').collection('cameras')
+
+for building_name, floors in data.items():
+    building_doc = cameras_col.document(building_name)
+    for floor_name, cameras in floors.items():
+        floor_col = building_doc.collection(floor_name)
+        
+        if cameras:
+            for cam in cameras:
+                cam_doc = floor_col.document(cam['name'])
+                batch.set(cam_doc, cam)
+        else:
+            placeholder = floor_col.document('no cameras')
+            batch.set(placeholder, {})
+
 
 batch.commit()
+
 print("Uploaded initial camera info to Firebase")
