b3cd31bac405c5d8880d9611a9e136034c725ee0
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-04-27 21:05:06 -0400

    change firebase cameras format

--- process.py
+++ process.py
@@ -2,7 +2,7 @@ import cv2
 
 from detect import detect
 
-def process(rtsp_stream, db, school, building, floor, cam_id):    
-    detect(rtsp_stream, db, school, building, floor, cam_id)
-    rtsp_stream.stop()
+def process(stream, db, cam_id, school):    
+    detect(stream, db, cam_id, school)
+    stream.stop()
     cv2.destroyAllWindows()
