8604a0c7255c12bc87075119de8cb9c30b329b04
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-04-28 20:54:33 -0400

    break incase too many invalid frames

--- process.py
+++ process.py
@@ -1,34 +1,40 @@
-# process.py
-import cv2
 import time
-from multiprocessing import Process, Queue
+from multiprocessing import Process
+from multiprocessing import Queue
 
 from detect import detect_worker
 from record import record_worker
 from stream import RTSPStream
 
-def frame_reader(rtsp_url, q_detect, q_record):
+def frame_reader(rtsp_url, cam_id, q_detect, q_record):
     stream = RTSPStream(rtsp_url)
-    
+    INVALID_FRAME_COUNT = 0
+
     while True:
         frame = stream.read()
         
         if frame is not None:
             q_detect.put(frame)
             q_record.put(frame)
+            INVALID_FRAME_COUNT = 0
         else:
             print("INVALID FRAME")
+            INVALID_FRAME_COUNT += 1
             time.sleep(0.1)
+            
+            if INVALID_FRAME_COUNT == 10:
+                break
 
+    print(f"\nTOO MANY INVALID FRAMES: {cam_id} CAMERA STREAM ENDED\n")
     stream.stop()
 
 def process(rtsp_url, cam_id, school):
-    q_detect = Queue(maxsize=32)   # bounded so we don’t OOM
+    q_detect = Queue(maxsize=32)
     q_record = Queue(maxsize=32)
 
-    p_read   = Process(target=frame_reader, args=(rtsp_url, q_detect, q_record), name="reader")
-    p_detect = Process(target=detect_worker, args=(q_detect, cam_id, school),    name="detector")
-    p_record = Process(target=record_worker, args=(q_record, cam_id),            name="recorder")
+    p_read = Process(target=frame_reader, args=(rtsp_url, cam_id, q_detect, q_record), name="reader")
+    p_detect = Process(target=detect_worker, args=(q_detect, cam_id, school), name="detector")
+    p_record = Process(target=record_worker, args=(q_record, cam_id), name="recorder")
     
     p_read.start()
     p_detect.start()
