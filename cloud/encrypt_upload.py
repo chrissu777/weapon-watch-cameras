b5f5068a3649c6210820f1d33cb5a10001e5374d
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-05-03 01:20:09 -0400

    update

--- cloud/encrypt_upload.py
+++ cloud/encrypt_upload.py
@@ -1,48 +1,61 @@
 import boto3
+from datetime import datetime
+
 import aws_encryption_sdk
 from aws_encryption_sdk.identifiers import CommitmentPolicy
 from aws_encryption_sdk.streaming_client import StreamEncryptor
+
+from aws_cryptographic_material_providers.mpl.models import CreateAwsKmsKeyringInput
 from aws_cryptographic_material_providers.mpl import AwsCryptographicMaterialProviders
 from aws_cryptographic_material_providers.mpl.config import MaterialProvidersConfig
 from aws_cryptographic_material_providers.mpl.models import CreateAwsKmsKeyringInput
 from aws_cryptographic_material_providers.mpl.references import IKeyring
 
-import config
+from cloud.config import KMS_KEY_ARN
 
-def encrypt_and_upload(file_path, s3_key):
+S3_BUCKET = 'weaponwatch-demo'
+s3_client = boto3.client('s3')
+
+def encrypt_and_upload(file_path, s3_key, cam_name):
+    """Encrypts file using AWS KMS and uploads to provided S3 bucket"""
+    
+    # Initialize AWS Encryption SDK client
     client = aws_encryption_sdk.EncryptionSDKClient(
         commitment_policy=CommitmentPolicy.REQUIRE_ENCRYPT_REQUIRE_DECRYPT
     )
+
+    # Create KMS client for encryption
     kms_client = boto3.client('kms', region_name="us-east-1")
 
-    mat_prov = AwsCryptographicMaterialProviders(
-    config=MaterialProvidersConfig())
-    kms_keyring_input = CreateAwsKmsKeyringInput(
-        kms_key_id="arn:aws:kms:us-east-1:203918845922:key/f0e18996-d4a0-49f6-827d-cc8915c5f864",
+    # Configure cryptographic material providers
+    mat_prov: AwsCryptographicMaterialProviders = AwsCryptographicMaterialProviders(
+        config=MaterialProvidersConfig()
+    )
+
+    # Create AWS KMS keyring for encryption
+    keyring_input: CreateAwsKmsKeyringInput = CreateAwsKmsKeyringInput(
+        kms_key_id=KMS_KEY_ARN,
         kms_client=kms_client
     )
     kms_keyring: IKeyring = mat_prov.create_aws_kms_keyring(
-        input=kms_keyring_input
+        input=keyring_input
     )
-    s3_client = boto3.client("s3", region_name=config.AWS_REGION)
 
-    ciphertext_buffer = bytearray()
     try:
+        # Read file and encrypt contents
         with open(file_path, "rb") as infile:
-            plaintext = infile.read()
+            ciphertext, _ = client.encrypt(
+                source=infile,
+                keyring=kms_keyring,
+            )
 
-        ciphertext, encrypt_header = client.encrypt(
-            source=plaintext,
-            keyring=kms_keyring
-        )
-
-
-        s3_client.put_object(
-            Bucket=config.S3_BUCKET,
-            Key=s3_key,
-            Body=bytes(ciphertext_buffer)
-        )
-        print(f"Encrypted file uploaded successfully to s3://{config.S3_BUCKET}/{s3_key}")
+        # Upload encrypted data to S3
+        s3_client.put_object(Bucket=S3_BUCKET, Key=s3_key, Body=bytes(ciphertext))
+        
+        print(f"\nENCRYPTED FILE UPLOADED SUCCESSFULLY: {s3_key}")
+        
+        formatted_time = datetime.now().strftime("%H:%M:%S")
+        print(f"UPLOADED AT {formatted_time} FOR {cam_name}")
 
     except Exception as e:
-        print(f"Error during encryption or upload: {e}")
+        print(f"Error: {str(e)}")  # Print error if encryption or upload fails
