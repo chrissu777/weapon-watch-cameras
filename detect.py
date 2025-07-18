f2cca0693437e2b42b602814267324808e4d0582
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2025-07-18 13:33:34 -0400

    change to finals verificaiton format"

--- detect.py
+++ detect.py
@@ -11,12 +11,11 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
 
-def detect(frame, cam_id, cam_name, detection_model, blob, school_ref, cam_ref, buffer):
+def detect(frame, cam_id, cam_name, infer_weapon, buffer):
     image_data = cv2.resize(frame, (608, 608))
     image_data = image_data / 255.
     image_data = image_data[np.newaxis, ...].astype(np.float32)
 
-    infer_weapon = detection_model.signatures['serving_default']
     batch_data = tf.constant(image_data)
     pred_bbox = infer_weapon(batch_data)
 
@@ -38,25 +37,25 @@ def detect(frame, cam_id, cam_name, detection_model, blob, school_ref, cam_ref, 
 
     if valid_detections:
         print(f"\nWEAPON DETECTED: {cam_name}")
-        school_ref.update({"detected_cam_id": cam_id})
-        cam_ref.update({"detected": True})
+        # school_ref.update({"detected_cam_id": cam_id})
+        # cam_ref.update({"detected": True})
                     
         original_h, original_w, _ = frame.shape
         bboxes = utils.format_boxes(boxes.numpy()[0][:valid_detections], original_h, original_w)
-        cam_ref.update({"bboxes": bboxes.flatten().tolist()})
+        # cam_ref.update({"bboxes": bboxes.flatten().tolist()})
 
         pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections]
         frame = utils.draw_bbox(frame, pred_bbox, info=False)
 
-        image_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        image_pil.save(buffer, format="JPEG")
-        buffer.seek(0)
-        blob.upload_from_file(buffer, content_type="image/jpeg")
-        print("DETECTED PHOTO UPLOADED TO FIREBASE")
-    else:
+        # image_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        # image_pil.save(buffer, format="JPEG")
+        # buffer.seek(0)
+        # blob.upload_from_file(buffer, content_type="image/jpeg")
+        # print("DETECTED PHOTO UPLOADED TO FIREBASE")
+    # else:
         # print(f"\nNO WEAPON DETECTED: {cam_name}")
-        cam_ref.update({"detected": False})
-        cam_ref.update({"bboxes": [0, 0, 0, 0]})
+        # cam_ref.update({"detected": False})
+        # cam_ref.update({"bboxes": [0, 0, 0, 0]})
         # school_ref.update({"detected_cam_id": ""})
     
     # if frame is not None and frame.size > 0:
@@ -70,22 +69,23 @@ def detect(frame, cam_id, cam_name, detection_model, blob, school_ref, cam_ref, 
 
     
 def detect_worker(q_detect, cam_id, cam_name, school, detection_model):
-    if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
-        firebase_admin.initialize_app(cred, {
-            "storageBucket": "weapon-watch.firebasestorage.app"
-        })
+    # if not firebase_admin._apps:
+    #     cred = credentials.Certificate("serviceAccountKey.json")
+    #     firebase_admin.initialize_app(cred, {
+    #         "storageBucket": "weapon-watch.firebasestorage.app"
+    #     })
 
-    db = firestore.client()
-    bucket = storage.bucket()
-    blob = bucket.blob(f"frame_for_verifier*{cam_id}.jpg")
+    # db = firestore.client()
+    # bucket = storage.bucket()
+    # blob = bucket.blob(f"frame_for_verifier*{cam_id}.jpg")
 
-    school_ref = db.collection("schools").document(school)
-    cam_ref = school_ref.collection("cameras").document(cam_id)
+    # school_ref = db.collection("schools").document(school)
+    # cam_ref = school_ref.collection("cameras").document(cam_id)
     buffer = io.BytesIO()
 
     print(f"DETECTION WORKER READY FOR {cam_name}")
     
     while True:
         frame = q_detect.get()    # blocks until a frame arrives
-        detect(frame, cam_id, cam_name, detection_model, blob, school_ref, cam_ref, buffer)
+        # detect(frame, cam_id, cam_name, detection_model, blob, school_ref, cam_ref, buffer)
+        detect(frame, cam_id, cam_name, detection_model, buffer)
